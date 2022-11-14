8f4d0d70421553eeb4c9f56ea7df801bea1548db
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2022-11-14 20:36:19 +0100

    completed code

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Dopo inseriti i 5 numeri bisogna dire quanti e quali numeri sono stati indovinati.
+
+const arrayRandom = getArrayRandom();
+
+let trovato = 0;
+
+
+const textEl = document.querySelector(".row :nth-child(2)")
+
+
+console.log(arrayRandom);
+
+
+
+generateArrayEl(textEl, arrayRandom);
+
+
+let arrayInput = [];
+
+
+setTimeout(() => {
+
+    getNumber(arrayInput, arrayRandom);
+
+    console.log(arrayInput)
+
+    console.log(trovato);
+
+    if (trovato === 1) {
+        alert(`Hai trovato la bellezza di 1 numero`)
+
+    } else if (trovato > 1) {
+
+        alert(`Hai trovato la bellezza di ${trovato} numeri`)
+
+    } else if (trovato == 0) {
+
+        alert(`Hai trovato numeri ${trovato} numeri... dai riprova !`)
+
+    }
+
+}, 12000);
+
---
+++ js/utilities.js
@@ -0,0 +1,68 @@
+function generateRandomNumber() {
+    num = Math.floor(Math.random() * 50) + 1;
+    return num;
+}
+
+function getArrayRandom() {
+
+    const arrayRandom = [];
+
+    while (arrayRandom.length < 5) {
+
+        const num = generateRandomNumber();
+
+        if (!arrayRandom.includes(num)) {
+
+            arrayRandom.push(num);
+
+        }
+    }
+
+    return arrayRandom;
+}
+
+function generateArrayEl(arrayEl, arrayNum) {
+
+    for (let i = 0; i < 5; i++) {
+
+        const spanEl = document.createElement("span");
+
+        spanEl.innerHTML = ` ${arrayNum[i]} `;
+
+        arrayEl.append(spanEl)
+
+        setTimeout(() => {
+
+            spanEl.innerHTML = "";
+
+        }, 10000);
+
+    }
+
+}
+
+function getNumber(input, arrayNum) {
+
+    let i = 0;
+
+    while (i < 5) {
+
+        const numInput = parseInt(prompt("Inserisci il numero :"));
+
+        for (let j = 0; j < 5; j++) {
+
+            if (numInput == arrayNum[j]) {
+
+                trovato++;
+
+            }
+        }
+
+        input.push(numInput);
+
+        i++;
+    }
+
+    return input;
+
+}
